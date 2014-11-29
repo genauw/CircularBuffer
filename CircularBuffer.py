4a69de0981c8ef22e2a3ac5b6cc8d34ebb990b2f
David Lutkin <genauw@gmail.com>
2014-11-29 20:05:52 +0100

    Add comments + stuff for p3

--- CircularBuffer.py
+++ CircularBuffer.py
@@ -4,8 +4,8 @@ from collections import deque
 
 # General function to get input from the command line
 def getInput():
-		i = raw_input("-->")
-		return i 
+		i = raw_input("-->") #find out what raw_input is for python3 !!!!!!!!!!!
+		return i
 
 # Class to hold an instance of a circular buffer
 # Requires an input integer to set the max number of objects the buffer can hold
@@ -17,7 +17,7 @@ class CircularBuffer:
 	# method to return the length of the buffer
 	def size(self):
 		return len(self.bufferList)
-	
+
 	# List of possible valid commands to receive from input
 	# each of the three commands
 	def inputCommands(self, inputString):
@@ -30,7 +30,7 @@ class CircularBuffer:
 		elif (a[0] == commandArray[2]):
 			self.lister()
 		else:
-			print "invalid input, please try again"
+			print ("invalid input, please try again")
 
 	# Adds n number of inputs to the buffer
 	# Adds length of characters in buffer to totalChar ??????????? <-------------
@@ -43,18 +43,18 @@ class CircularBuffer:
 			else:
 				self.totalChar = self.totalChar - len(g)
 				break
-				
+
 	def remover(self, n):
 		for i in range(0, int(n)):
 			self.bufferList.popleft()
 
 	def lister(self):
 		for i in range(0, self.size()):
-			print self.bufferList[i]
+			print (self.bufferList[i])
 
 	def charLimitChecker(self):
 		if (self.totalChar >= 20):
-			print "There are too many characters stored, please remove something to continue"
+			print ("There are too many characters stored, please remove something to continue")
 			return False
 		else:
 			return True
@@ -66,7 +66,7 @@ while (True):
 		x = CircularBuffer(i)
 		break
 	else:
-		print "please keep buffer size between 0 and 10000, inclusive"
+		print ("please keep buffer size between 0 and 10000, inclusive")
 
 while (True):
 	g = getInput()
